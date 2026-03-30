7b6c8a8f61d93ad91e8906ccb5d60908b8a7e336
Kavya Hosamane Jayanna <125850274+kavyahj04@users.noreply.github.com>
2026-03-30 10:18:05 -0400

    1

--- #1_Contains_Duplicate.py
+++ #1_Contains_Duplicate.py
@@ -1,17 +1,22 @@
-def containsDuplicate(nums):
-    hashmap = {}
-    for i in range(len(nums)):
-        if nums[i] in hashmap:
+#Given an integer array nums, return true if any value appears at least twice in the array, 
+# and return false if every element is distinct.
+
+# Input: nums = [1,2,3,1]
+
+# Output: true
+
+def contains_duplicate(nums):
+    nums_map = {}
+    for n in nums:
+        nums_map[n] = 1 + nums_map.get(n, 0)
+    
+    for key, value in nums_map.items():
+        if value > 1:
             print("True")
             return True
-        hashmap[nums[i]] = i
     print("False")
-    return False
+    return False 
 
-# numbers = [1,2, 4, 5, 7]
-numbers = [1,2, 4, 5, 7, 1]
-
-containsDuplicate(numbers)
-
-#TC - O(n)
-#SC - O(n) - because we are using hashmap
+nums = [1, 2, 3, 4, 1]
+nums = [1, 2, 3, 4]
+contains_duplicate(nums)
