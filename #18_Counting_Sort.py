eec13ab0b4020c5d3c2da71ef816e4633beeb207
Kavya Hosamane Jayanna <125850274+kavyahj04@users.noreply.github.com>
2026-04-06 10:43:48 -0400

    counting sort

---
+++ #18_Counting_Sort.py
@@ -0,0 +1,17 @@
+def countingSort(nums):
+    max_val = max(nums)
+    count = [0] * (max_val + 1)
+
+    for n in nums:
+        count[n] += 1
+
+    k = 0
+    for i, freq in enumerate(count):
+        for _ in range(freq):
+            nums[k] = i
+            k+=1
+    print(nums)
+
+
+nums = [2,0,2,1,1,0]
+countingSort(nums)
