844b991e1a6d9fa873a729f40fd1268f18620b72
Kavya Hosamane Jayanna <125850274+kavyahj04@users.noreply.github.com>
2026-04-16 08:34:33 -0400

    longest subsequence

---
+++ #27_longestSubsequence.py
@@ -0,0 +1,16 @@
+def longestSubSequence(s):
+    l = 0
+    res = 0
+    charSet = set()
+
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l+=1
+        charSet.add(s[r])
+        res = max(res, r-l+1)
+    print(res)
+    return res
+
+s = "pwwkew"
+longestSubSequence(s)
