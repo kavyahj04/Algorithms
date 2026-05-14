2ecd8614cab7abffa0ca82d9a0c68f40dc8400ef
Kavya Hosamane Jayanna <125850274+kavyahj04@users.noreply.github.com>
2026-05-14 08:54:53 -0400

    ship withing n days

--- #49_capacity_weights.py
+++ #49_capacity_weights.py
@@ -1,14 +1,28 @@
-def capCheck(c):
-    sum_ = 0
-    d = 1
-    for i in range(len(weights)):
-        if sum_ + weights[i] <= c:
-            sum_ += weights[i]
-        else:
-            d = d+1
-            sum_ = 0
-            sum_ += weights[i]
-    if d <= days:
-        return True
-    else:
-        return False 
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def capCheck(c):
+            s, d = 0, 1
+            for i in range(len(weights)):
+                if s + weights[i] <= c:
+                    s += weights[i]
+                else:
+                    s, d = 0, d + 1
+                    s += weights[i]
+            if d <= days:
+                return True
+            else:
+                return False
+        
+        min_cap = max(weights)
+        max_cap = sum(weights)
+        cap = False
+        res = min_cap
+        while min_cap <= max_cap:
+            mid = (min_cap + max_cap) // 2
+            cap = capCheck(mid)
+            if cap:
+                res = mid
+                max_cap = mid - 1
+            else:
+                min_cap = mid + 1
+        return res
